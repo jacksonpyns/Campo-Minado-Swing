e8e9568bf7cb15ee70429773158a7f12a3c0e9e7
Jackson <limajackson115@gmail.com>
2026-09-05 00:34:51 -0300

    Interface Gráfica do Campo Minado #01

---
+++ src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,18 @@
+package br.com.cod3r.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	/*
+	 *  É um componente que agrupa outros componentes dentro dele,
+	 * então, dentro dele tem uma serie de botões, e cada botão
+	 * representando um canto do jogo.
+	 */
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module campo_minado_swing {
+	requires java.desktop;
 }
