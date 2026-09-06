5a84315f4522a9a681d5fa06a00ea0ceb56c1069
Jackson <limajackson115@gmail.com>
2026-09-06 01:33:59 -0300

    Interface Gráfica do Campo Minado #06

--- src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -51,20 +51,32 @@ public class BotaoCampo extends JButton
 	}
 
 	private void aplicarEstiloPadrao() {
-		
+		setBackground(BG_PADRAO);
+		setText("");
 	}
 
 	private void aplicarEstiloExplodir() {
-		
+		setBackground(BG_EXPLODIR);
+		setForeground(Color.WHITE);
+		setText("X");
 	}
 
 	private void aplicarEstiloMarcar() {
-		
+		setBackground(BG_MARCAR);
+		setForeground(Color.BLACK);
+		setText("M");
 	}
 
 	private void aplicarEstiloAbrir() {
-		setBackground(BG_PADRAO);
+		
 		setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		
+		if (campo.isMinado()) {
+			setBackground(BG_EXPLODIR);
+			return;
+		}
+		
+		setBackground(BG_PADRAO);
 
 		switch (campo.minasNaVizinhanca()) {
 		case 1:
