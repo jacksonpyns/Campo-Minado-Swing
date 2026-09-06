dd270e4477496d10bb83350f846010c124b5a3b7
Jackson <limajackson115@gmail.com>
2026-09-06 00:47:59 -0300

    Interface Gráfica do Campo Minado #03 / Cores botões

--- src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -1,14 +1,64 @@
 package br.com.cod3r.cm.visao;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 import br.com.cod3r.cm.modelo.Campo;
+import br.com.cod3r.cm.modelo.CampoEvento;
+import br.com.cod3r.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
 
+	// Essas cores são 1 = Red, 2 = Grenn, 3 = Blue = RGB
+	private final Color BG_PADRAO = new Color(184, 184, 184);
+	private final Color BG_MARCAR = new Color(8, 179, 247);
+	private final Color BG_EXPLODIR = new Color(189, 66, 68);
+	private final Color BG_VERDE = new Color(0, 100, 0);
+	
+	private Campo campo;
+	
 	// Aqui vai mexer em todos os botões
 	public BotaoCampo(Campo campo) {
+		this.campo = campo;
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createBevelBorder(0));
+		
+		campo.registrarObservador(this);
+	}
+	
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch(evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+		default:
+			aplicarEstiloPadrao();
+		}
+	}
+
+	private void aplicarEstiloPadrao() {
+		
+	}
+
+	private void aplicarEstiloExplodir() {
+		
+	}
+
+	private void aplicarEstiloMarcar() {
+		
+	}
+
+	private void aplicarEstiloAbrir() {
 		
 	}
 }
